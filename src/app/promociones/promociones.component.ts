00b7dda25e454d2dab754f0e48b57ed1062ab2d1
Luis <luisale9826@gmail.com>
2020-12-07 20:46:24 -0600

    Arreglos

--- src/app/promociones/promociones.component.ts
+++ src/app/promociones/promociones.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
-import { SwiperComponent, SwiperDirective } from 'ngx-swiper-wrapper';
-import { SwiperOptions } from 'swiper';
-import { AutoplayOptions } from 'swiper/types/components/autoplay';
-import { PaginationOptions } from 'swiper/types/components/pagination';
-import { ScrollbarOptions } from 'swiper/types/components/scrollbar';
 import { Promocion } from '../models/Promocion';
 import { PromocionService } from '../services/promocion.service';
 
